0f60a3ee370b83aa3602ded80ae09837992ddf6b
Ana Cristina Martinez <am8794041@gmail.com>
2024-12-03 22:11:38 -0600

    seguridad

--- src/main/java/artesanas/artesanas/SecurityConfig.java
+++ src/main/java/artesanas/artesanas/SecurityConfig.java
@@ -14,46 +14,18 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
-    /*
-     * @Bean
-     * public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-     * http.csrf(AbstractHttpConfigurer::disable)
-     * .authorizeHttpRequests(
-     * auth -> auth.requestMatchers("/signin", "/signup").permitAll()
-     * .requestMatchers("/customers/**", "/payments/**",
-     * "/shoppings/**","carts/**","/shippings/**","/addresses/**",
-     * "/productsCarts/**").hasRole("ADMIN")
-     * .requestMatchers("/").hasRole("USER")
-     * .anyRequest().authenticated())
-     * .formLogin(withDefaults())
-     * .rememberMe(withDefaults())
-     * .logout(logout -> logout.logoutUrl("/signout").permitAll());
-     * return http.build();
-     * }
-     */
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable) // Deshabilitar CSRF si no es necesario
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/signin", "/signup").permitAll() // Permitir acceso sin autenticación
-                        .requestMatchers("/customers/**", "/payments/**", "/shoppings/**", "/carts/**", "/shippings/**",
-                                "/addresses/**", "/productsCarts/**")
-                        .hasRole("ADMIN")
-                        .requestMatchers("/doc/swagger-ui/**").authenticated() // Requerir autenticación para Swagger
-                        .requestMatchers("/").authenticated() // Requerir autenticación para la raíz
-                        .anyRequest().authenticated() // Todas las demás rutas requieren autenticación
-                )
-                .formLogin(form -> form
-                        .loginPage("/signin") // Página de inicio de sesión personalizada
-                        .defaultSuccessUrl("/", true) // Redirigir a la raíz después de autenticarse
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/signout") // URL para cerrar sesión
-                        .logoutSuccessUrl("/signin") // Redirigir a la página de inicio de sesión tras cerrar sesión
-                        .permitAll())
-                .rememberMe(withDefaults()); // Configuración de "recordar usuario"
-
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(
+                auth -> auth.requestMatchers("/signin", "/signup").permitAll()
+                    .requestMatchers("/customers/**", "/payments/**","/shoppings/**", "carts/**", "/shippings/**", "/addresses/**","/productsCarts/**").hasRole("ADMIN")
+                    .requestMatchers("/").hasRole("USER")
+                    .anyRequest().authenticated())
+            .formLogin(withDefaults())
+            .rememberMe(withDefaults())
+            .logout(logout -> logout.logoutUrl("/signout").permitAll());
         return http.build();
     }
 
